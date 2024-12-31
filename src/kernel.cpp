f7eb0aff1a10d3c5b306c4a6a0579eaf1b7efbe8
Simone3838 <simone.paolini14@gmail.com>
2024-12-31 18:13:00 +0100

    Create kernel.cpp

---
+++ src/kernel.cpp
@@ -0,0 +1,10 @@
+// kernel.cpp
+extern "C" void kernel_main() {
+    const char *str = "Hello, FruitOS!";
+    char *video_memory = (char*)0xb8000;
+    for (int i = 0; str[i] != '\0'; ++i) {
+        video_memory[i * 2] = str[i];
+        video_memory[i * 2 + 1] = 0x07; // Light grey on black
+    }
+    while (1);
+}

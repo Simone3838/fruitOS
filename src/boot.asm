fed41a17ebc072264912aaea025409b90d99dc3d
Simone3838 <simone.paolini14@gmail.com>
2024-12-31 21:37:00 +0100

    Update boot.asm

--- src/boot.asm
+++ src/boot.asm
@@ -12,19 +12,57 @@ start:
     mov sp, 0x7C00
     sti
 
-    ; Print "fruitOS" to the screen
-    mov si, msg
+    ; Print "Loading kernel..." to the screen
+    mov si, msg_loading
 print_char:
     lodsb
     or al, al
-    jz done
+    jz done_print
     mov ah, 0x0E
     int 0x10
     jmp print_char
-done:
+done_print:
+
+    ; Load the kernel (512 bytes, one sector) from the second sector of the disk
+    mov ah, 0x02  ; BIOS read sector function
+    mov al, 1     ; Number of sectors to read
+    mov ch, 0     ; Cylinder number
+    mov cl, 2     ; Sector number (second sector)
+    mov dh, 0     ; Head number
+    mov dl, 0     ; Drive number (first floppy disk)
+    mov bx, 0x1000 ; Address to load the kernel (0x1000:0x0000)
+    int 0x13      ; BIOS interrupt
+
+    ; Check for errors
+    jc disk_error
+
+    ; Print "Kernel loaded" to the screen
+    mov si, msg_loaded
+    call print_string
+
+    ; Jump to the kernel
+    jmp 0x1000:0000
+
+disk_error:
+    ; Print "Disk error" to the screen
+    mov si, msg_error
+    call print_string
     hlt
 
-msg db 'fruitOS', 0
+print_string:
+print_char_loop:
+    lodsb
+    or al, al
+    jz done_string
+    mov ah, 0x0E
+    int 0x10
+    jmp print_char_loop
+done_string:
+    ret
+
+msg_loading db 'Loading kernel...', 0
+msg_loaded db 'Kernel loaded', 0
+msg_error db 'Disk error', 0
 
 times 510-($-$$) db 0
 dw 0xAA55

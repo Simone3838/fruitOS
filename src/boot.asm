ad852c0b599066d34b1feaeebed04e47581854f0
Simone3838 <simone.paolini14@gmail.com>
2024-12-31 18:33:27 +0100

    Update boot.asm

--- src/boot.asm
+++ src/boot.asm
@@ -3,8 +3,28 @@ BITS 16
 ORG 0x7C00
 
 start:
-    cli             ; Clear interrupts
-    hlt             ; Halt CPU
+    ; Set up the stack
+    cli
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
+    sti
 
-times 510 - ($ - $$) db 0  ; Fill the rest of the sector with zeros
-dw 0xAA55                  ; Boot signature
+    ; Print "fruitOS" to the screen
+    mov si, msg
+print_char:
+    lodsb
+    or al, al
+    jz done
+    mov ah, 0x0E
+    int 0x10
+    jmp print_char
+done:
+    hlt
+
+msg db 'fruitOS', 0
+
+times 510-($-$$) db 0
+dw 0xAA55

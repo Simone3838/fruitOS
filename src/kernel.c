397a3c96a093ca8ba4ff9c8c6a68f95d7076aa68
Simone3838 <simone.paolini14@gmail.com>
2025-01-01 10:06:48 +0100

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -1,26 +1,12 @@
 // kernel.c
+#include <stdint.h>
+
+// Function prototype
+void shell();
+
 void kernel_main(void) {
-    const char *str = "Welcome to fruitOS!";
-    char *vidptr = (char*)0xb8000;  // Video memory address
-    unsigned int i = 0;
-    unsigned int j = 0;
+    // Initialize the shell
+    shell();
 
-    // Clear the screen
-    while(j < 80 * 25 * 2) {
-        vidptr[j] = ' ';
-        vidptr[j+1] = 0x07;
-        j += 2;
-    }
-
-    j = 0;
-
-    // Write the string to video memory
-    while(str[j] != '\0') {
-        vidptr[i] = str[j];
-        vidptr[i+1] = 0x07;  // Attribute byte - white text on black background
-        ++j;
-        i += 2;
-    }
-
-    return;
+    // The shell function will take over, so no further code is needed here
 }
